268797c1ecfa873ecd42d0e831e0408e4b863521
shoaib <shobekhan@yahoo.com>
2026-02-26 23:57:51 +0100

    initial commit

--- src/api.js
+++ src/api.js
@@ -1,9 +1,4 @@
 export async function fetchUser(id) {
     const response = await fetch(`/api/users/${id}`);
-
-    if (!response.ok) {
-        throw new Error("Failed to fetch user");
-    }
-
     return response.json();
 }
