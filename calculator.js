d166e6a7df7976d04a7936541361e318e765850f
shoaib <shobekhan@yahoo.com>
2026-07-11 13:02:45 +0200

    jsdoc added better version

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
-// added jsdoc
+/**
+ * Adds two numbers
+ * @param {number} one - The first number
+ * @param {number} two - The second number
+ * @returns {number} The sum of the two numbers
+ */
 function add(one, two) {
     return one + two;
 }
