c7bdf8b635e3d710548fcc6a2957e711d1c01bdb
shoaib <shobekhan@yahoo.com>
2026-08-30 00:21:57 +0200

    running scripts locally

--- src/data/orderData.ts
+++
@@ -1,8 +0,0 @@
-import { Order } from "../services/orderService";
-
-export const sampleOrder: Order = {
-  id: "ORD-1001",
-  product: "Laptop",
-  price: 1200,
-  quantity: 1,
-};
--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import { sampleOrder } from "./data/orderData";
-import { createOrder } from "./services/orderService";
-
-console.log(createOrder(sampleOrder));
